9ad1989d1e200cd6df565160ea22af588b6d7fe0
Aaron I Trierweiler <aaronitrierweiler@MacBook-Pro.local>
2017-03-13 21:06:54 -0500

    added firebase config

---
+++ assets/logic.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAkteqISUm9sVzibyuHXcpfMtsFku141Vk",
+    authDomain: "body-shop-group-project.firebaseapp.com",
+    databaseURL: "https://body-shop-group-project.firebaseio.com",
+    storageBucket: "body-shop-group-project.appspot.com",
+    messagingSenderId: "761861202614"
+  };
+  firebase.initializeApp(config);
