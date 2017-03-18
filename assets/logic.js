81f70e79899ef92c1a4804098f0b0259b358d8a0
linhng15 <nguyett@lakeforest.edu>
2017-03-18 10:12:13 -0500

    adding aaron stuff

--- assets/logic.js
+++ assets/logic.js
@@ -91,6 +91,104 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
 // 4. Create a way to calculate the sum for all the work
 
+// Submit button Button
+// <button class="btn btn-primary" id="add-job-btn" type="submit">Submit</button>
+
+// User input
+// var projMetalLabor = $("#metalLabor-input").val().trim();
+
+
+// Make our variables global to the runtime of our application
+//     var firstNumber;
+//     var secondNumber;
+//     var operator;
+//     var result;
+//     var isOperatorChosen;
+//     var isCalculated;
+
+//     // Use a function to initialize our calculator.
+//     // This way when the user hits clear, we can guarantee a reset of the app.
+//     function initializeCalculator() {
+//       firstNumber = "";
+//       secondNumber = "";
+//       operator = "";
+//       isOperatorChosen = false;
+//       isCalculated = false;
+
+//       $("#first-number, #second-number, #operator, #result").empty();
+//     }
+
+//     // Add an on click listener to all elements that have the class "number"
+//     $(".number").on("click", function() {
+
+//       // Check if we've already run a calculation, if so... we'll just.
+//       if (isCalculated) return;
+
+//       // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
+//       if (isOperatorChosen) {
+//         secondNumber += this.value;
+//         $("#second-number").html(secondNumber);
+
+//       }
+//       else {
+//         firstNumber += this.value;
+//         $("#first-number").html(firstNumber);
+//       }
+//     });
+
+//     // Add an on click listener to all elements that have the class "operator"
+//     $(".operator").on("click", function() {
+
+//       // Check if we've already run a calculation, if so... we'll just exit.
+//       if (isCalculated) return;
+
+//       // Set isOperatorChosen to true so we start writing to secondNumber
+//       isOperatorChosen = true;
+
+//       // Store off the operator
+//       operator = this.value;
+
+//       // Set the HTML of the #operator to the text of what was clicked
+//       $("#operator").html($(this).text());
+
+//     });
+
+
+//     // Add an on click listener to all elements that have the class "equal"
+//     $(".equal").on("click", function() {
+
+//       // If we already clicked equal, don't do the calculation again
+//       if (isCalculated) return;
+
+//       // Set isCalculated to true so that we don't get in a weird UI state by clicking buttons again
+//       isCalculated = true;
+
+//       // Use parseInt to convert our string representation of numbers into actual integers
+//       firstNumber = parseInt(firstNumber);
+//       secondNumber = parseInt(secondNumber);
+
+//       // Based on the operator that was chosen.
+//       // Then run the operation and set the HTML of the result of that operation
+//       if (operator === "plus") {
+//         result = firstNumber + secondNumber;
+//       }
+
+//       $("#result").html(result);
+//     });
+
+//     // // Add an on click listener to all elements that have the class "clear"
+//     // $(".clear").on("click", function() {
+
+//       // Call initializeCalculater so we can reset the state of our app
+//       initializeCalculator();
+
+//     };
+
+//     // Call initializeCalculater so we can set the state of our app
+//     initializeCalculator();
+//   };
+
+
 //counter
 var sumMetalLabor= 0;
 
