1402390e2f979ab857bb1b4d1caf7ee5d754f9d8
linhng15 <nguyett@lakeforest.edu>
2017-03-24 18:24:19 -0500

    pushing

--- assets/logic.js
+++ assets/logic.js
@@ -25,14 +25,15 @@ var database = firebase.database();
 
 
 // Authentication
+
 var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup("provider").then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
     // ...
-    $("#.material-icons").append("user");
+    // $(".material-icons").append("user");
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
