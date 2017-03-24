2efd1030a13f821c5cb69d2a9d3cd1d93f0e90bb
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-03-24 15:21:26 -0500

    added authentication

--- assets/logic.js
+++ assets/logic.js
@@ -22,6 +22,29 @@ firebase.initializeApp(config);
 
 // Gets the Database service for the default app or a given app
 var database = firebase.database();
+
+
+// Authentication
+var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log(provider);
+  });
+
+
 //PANEL 3
 // 2. Submit Button for adding job labors
 $("#add-job-btn").on("click", function(event) {
